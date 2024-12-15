42a10598ed401377bbab9c1b2e31c1577401b83d
zhoujingya <jing.zhou@terapines.com>
2024-12-15 16:03:22 +0800

    feat: Add variable and function

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -1,5 +1,9 @@
 // TODO: Add some function with the name `call_me` without arguments or a return value.
 
+fn call_me() -> i32 {
+    return 1;
+}
+
 fn main() {
     call_me(); // Don't change this line
 }
