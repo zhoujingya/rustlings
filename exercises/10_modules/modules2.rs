957c07ca6cf2c0c8a42cd73aa6e7429f8ccf17ba
zhoujingya <jing.zhou@terapines.com>
2024-12-18 22:13:43 +0800

    feat: update module

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -3,15 +3,15 @@
 
 mod delicious_snacks {
     // TODO: Add the following two `use` statements after fixing them.
-    // use self::fruits::PEAR as ???;
-    // use self::veggies::CUCUMBER as ???;
+    pub use self::fruits::PEAR;
+    pub use self::veggies::CUCUMBER;
 
-    mod fruits {
+    pub mod fruits {
         pub const PEAR: &str = "Pear";
         pub const APPLE: &str = "Apple";
     }
 
-    mod veggies {
+    pub mod veggies {
         pub const CUCUMBER: &str = "Cucumber";
         pub const CARROT: &str = "Carrot";
     }
@@ -20,7 +20,7 @@ mod delicious_snacks {
 fn main() {
     println!(
         "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
-        delicious_snacks::veggie,
+        delicious_snacks::PEAR,
+        delicious_snacks::CUCUMBER,
     );
 }
